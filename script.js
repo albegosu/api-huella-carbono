ca8018761f21a63cd3ccf300a193a8f05db0e206
Alberto <albegosu@gmail.com>
2023-07-10 10:30:34 +0200

    Cambio a Asincrono

--- script.js
+++ script.js
@@ -2,27 +2,28 @@ let myHeaders = new Headers();
 myHeaders.append("Authorization", "Bearer E54V5B4NJ0MT8WM000NG0ZFY9J5K");
 myHeaders.append("Content-Type", "application/json");
 
-let raw = JSON.stringify({
-  "emission_factor": {
-    "activity_id": "heat_and_steam-type_district",
-    "data_version": "^1"
-  },
-  "parameters": {
-    "energy": 100,
-    "energy_unit": "kWh"
-  }
-});
+async function fetchData() {
+  try {
+    let raw = JSON.stringify({
+      "emission_factor": {
+        "activity_id": "heat_and_steam-type_district",
+        "data_version": "^1"
+      },
+      "parameters": {
+        "energy": 100,
+        "energy_unit": "kWh"
+      }
+    });
 
-let requestOptions = {
-  method: 'POST',
-  headers: myHeaders,
-  body: raw,
-  redirect: 'follow'
-};
+    let requestOptions = {
+      method: 'POST',
+      headers: myHeaders,
+      body: raw,
+      redirect: 'follow'
+    };
 
-fetch("https://beta4.api.climatiq.io/estimate", requestOptions)
-  .then(response => response.json())
-  .then(result => {
+    const response = await fetch("https://beta4.api.climatiq.io/estimate", requestOptions);
+    const result = await response.json();
     console.log(result);
 
     const dataTable = document.getElementById("data-table");
@@ -38,5 +39,9 @@ fetch("https://beta4.api.climatiq.io/estimate", requestOptions)
     row.appendChild(emissionsCell);
 
     dataTable.appendChild(row);
-  })
-  .catch(error => console.log('error', error));
+  } catch (error) {
+    console.log('error', error);
+  }
+}
+
+fetchData();
